5d36683c473968c8ce922fe108b5174223563779
tuckerbohman5 <tucker.bohman@gmail.com>
2016-03-19 12:34:36 -0600

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319173811) do
+ActiveRecord::Schema.define(version: 20160319183230) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
+    t.string  "status"
   end
 
   create_table "categories", force: :cascade do |t|
